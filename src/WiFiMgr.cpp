6680df68bc3200f452f05c01bbf58b46fc3360a7
Blackhack <davidaristi.0504@gmail.com>
2021-05-22 10:54:38 -0500

    Added initial source. Examples pending.

---
+++ src/WiFiMgr.cpp
@@ -0,0 +1,261 @@
+/*
+    WiFiMgr - ESP32 library to take care of WiFi connections/reconnections and NTP Time using its own thread
+
+    Copyright(C) 2021 Blackhack <davidaristi.0504@gmail.com>
+
+    This program is free software : you can redistribute it and /or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see < https://www.gnu.org/licenses/>.
+*/
+
+#include "WiFiMgr.h"
+#include "time.h"
+
+WiFiMgr::WiFiMgr()
+{
+    _time_management = false;
+    _time_initialized = false;
+
+    // Other variables are initialized at .begin() and EnableTimeMgr()
+}
+
+void WiFiMgr::begin(bool ap_mode, String ssid, String password)
+{
+    _running = true;
+    _ap_mode = ap_mode;
+    _ssid = ssid;
+    _password = password;
+    _state = WIFIMGR_INITIAL;
+
+    xTaskCreate(
+        WiFiMgr::RunWrapper,
+        "WiFiManager",
+        3000,
+        this,
+        1,
+        nullptr
+    );
+}
+
+void WiFiMgr::EnableTimeMgr(int32_t gmt_offset, int32_t daylight_offset, String ntp_server_1, String ntp_server_2, String ntp_server_3)
+{
+    _time_management = true;
+    _gmt_offset = gmt_offset;
+    _daylight_offset = daylight_offset;
+    _ntp_server_1 = ntp_server_1;
+    _ntp_server_2 = ntp_server_2;
+    _ntp_server_3 = ntp_server_3;
+}
+
+void WiFiMgr::Disconnect()
+{
+    _running = false;
+    _state = WIFIMGR_DISCONNECTED_REQUESTED;
+    WiFi.disconnect();
+
+    log_i("WiFi client mode disconnected as requested");
+}
+
+bool WiFiMgr::IsConnected()
+{
+    return WiFi.isConnected() && _state == WIFIMGR_CONNECTED;
+}
+
+String WiFiMgr::GetTimeFormat(String format)
+{
+    if (!_time_initialized)
+        return "NN:NN:NN";
+
+    tm timeinfo;
+    if (!getLocalTime(&timeinfo))
+    {
+        log_e("Failed to obtain time");
+        return "NN:NN:NN";
+    }
+
+    char buf[64];
+    size_t written = strftime(buf, 64, "%A, %B %d %Y %H:%M:%S", &timeinfo);
+    if (written == 0)
+    {
+        log_e("Failed to format time");
+        return "NN:NN:NN";
+    }
+
+    return String(buf);
+}
+
+tm WiFiMgr::GetTimeInfo()
+{
+    tm timeinfo = { 0 };
+
+    if (!_time_initialized)
+        return timeinfo;
+
+    if (!getLocalTime(&timeinfo))
+        log_e("Failed to obtain time");
+
+    return timeinfo;
+}
+
+void WiFiMgr::RunWrapper(void* parameter)
+{
+    static_cast<WiFiMgr*>(parameter)->Run();
+}
+
+void WiFiMgr::Run()
+{
+    uint32_t startUpdate = millis();
+    uint32_t EndUpdate = startUpdate;
+    uint32_t lastStartUpdate = startUpdate;
+
+    while (_running)
+    {
+        startUpdate = millis();
+        uint32_t diff = startUpdate - lastStartUpdate;
+        lastStartUpdate = startUpdate;
+
+        ReconnectAttempt(diff);
+        TimeCheck(diff);
+
+        EndUpdate = millis();
+        uint32_t updateThreadTime = EndUpdate - startUpdate;
+
+        if (updateThreadTime < sleep_timer_const)
+            vTaskDelay((sleep_timer_const - updateThreadTime) / portTICK_PERIOD_MS);
+    }
+
+    vTaskDelete(nullptr);
+}
+
+void WiFiMgr::Connect()
+{
+    WiFi.disconnect();
+
+    if (_ap_mode)
+    {
+        log_i("WiFi SoftAP mode initialized");
+        WiFi.softAP(_ssid.c_str(), _password.c_str());
+        _state = WIFIMGR_SOFT_AP;
+    }
+    else
+    {
+        log_i("WiFi client mode connection started");
+        WiFi.begin(_ssid.c_str(), _password.c_str());
+        _state = WIFIMGR_CONNECTING;
+    }
+}
+
+void WiFiMgr::ReconnectAttempt(uint32_t diff)
+{
+    static uint32_t reconnect_timer = 1000;
+    static uint32_t attempt_count = 1;
+
+    if (!_running || WiFi.isConnected())
+    {
+        if (_state == WIFIMGR_CONNECTING)
+        {
+            log_i("WiFi connected at attempt %u", attempt_count);
+            _state = WIFIMGR_CONNECTED;
+        }
+
+        reconnect_timer = 1000;
+        attempt_count = 1;
+
+        return;
+    }
+
+    if (_state == WIFIMGR_CONNECTED)
+    {
+        log_e("WiFi connection loss, attempting reconnection...");
+        log_i("Internal status: %s(%i)", StatusToString(WiFi.status()).c_str(), WiFi.status());
+        _state = WIFIMGR_DISCONNECTED_LOSS;
+    }
+
+    if (reconnect_timer <= diff)
+    {
+        if (_state == WIFIMGR_DISCONNECTED_LOSS || _state == WIFIMGR_INITIAL)
+            Connect();
+
+        log_i("Attempt number %u, internal status: %s(%i)", attempt_count, StatusToString(WiFi.status()).c_str(), WiFi.status());
+
+        if (attempt_count % 10 == 0)
+        {
+            log_e("Can't connect after %u attempts, retrying...", attempt_count);
+            Connect();
+        }
+
+        ++attempt_count;
+        reconnect_timer = 1000;
+    }
+    else reconnect_timer -= diff;
+}
+
+void WiFiMgr::TimeCheck(uint32_t diff)
+{
+    static uint32_t time_initialization_timer = 1000;
+
+    if (_time_initialized || !_time_management || !IsConnected())
+        return;
+
+    if (time_initialization_timer <= diff)
+    {
+        const char* server1 = _ntp_server_1.c_str();
+        const char* server2 = _ntp_server_2.length() > 0 ? _ntp_server_2.c_str() : nullptr;
+        const char* server3 = _ntp_server_3.length() > 0 ? _ntp_server_3.c_str() : nullptr;
+
+        configTime(_gmt_offset, _daylight_offset, server1, server2, server3);
+
+        tm timeinfo;
+        if (!getLocalTime(&timeinfo))
+            log_e("Failed to obtain time");
+        else
+        {
+            char buf[64];
+            size_t written = strftime(buf, 64, "%A, %B %d %Y %H:%M:%S", &timeinfo);
+            if (written != 0)
+                log_i("Time initialized at %s", buf);
+
+            _time_initialized = true;
+        }
+
+        time_initialization_timer = 1000;
+    }
+    else time_initialization_timer -= diff;
+}
+
+void WiFiMgr::Reboot()
+{
+    log_w("Rebooting ESP32...");
+    WiFi.disconnect();
+
+    delay(1000);
+    ESP.restart();
+}
+
+String WiFiMgr::StatusToString(wl_status_t status)
+{
+    switch (status)
+    {
+        case WL_NO_SHIELD: return "WL_NO_SHIELD";
+        case WL_IDLE_STATUS: return "WL_IDLE_STATUS";
+        case WL_NO_SSID_AVAIL: return "WL_NO_SSID_AVAIL";
+        case WL_SCAN_COMPLETED: return "WL_SCAN_COMPLETED";
+        case WL_CONNECTED: return "WL_CONNECTED";
+        case WL_CONNECT_FAILED: return "WL_CONNECT_FAILED";
+        case WL_CONNECTION_LOST: return "WL_CONNECTION_LOST";
+        case WL_DISCONNECTED: return "WL_DISCONNECTED";
+    }
+
+    return "WL_UNK";
+}
+
+WiFiMgr WiFiManager;
---
+++ src/WiFiMgr.h
@@ -0,0 +1,80 @@
+/*
+    WiFiMgr - ESP32 library to take care of WiFi connections/reconnections and NTP Time using its own thread
+
+    Copyright(C) 2021 Blackhack <davidaristi.0504@gmail.com>
+
+    This program is free software : you can redistribute it and /or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see < https://www.gnu.org/licenses/>.
+*/
+
+#ifndef _WIFIMANAGER_h
+#define _WIFIMANAGER_h
+
+#include "arduino.h"
+#include <WiFi.h>
+#include <atomic>
+
+const uint32_t sleep_timer_const = 500;
+
+class WiFiMgr
+{
+public:
+    WiFiMgr();
+
+    void begin(bool ap_mode, String ssid, String password);
+    void EnableTimeMgr(int32_t gmt_offset, int32_t daylight_offset, String ntp_server_1 = "pool.ntp.org", String ntp_server_2 = "", String ntp_server_3 = "");
+
+    void Disconnect();
+    bool IsConnected();
+
+    String GetTimeFormat(String format);
+    tm GetTimeInfo();
+private:
+    static void RunWrapper(void* parameter);
+    void Run();
+    void Connect();
+    void ReconnectAttempt(uint32_t diff);
+    void TimeCheck(uint32_t diff);
+    void Reboot();
+    String StatusToString(wl_status_t status);
+
+private:
+    std::atomic_bool _running;
+    bool _ap_mode;
+    String _ssid;
+    String _password;
+
+    enum WIFIMGR_STATE
+    {
+        WIFIMGR_INITIAL,
+        WIFIMGR_CONNECTING,
+        WIFIMGR_CONNECTED,
+        WIFIMGR_SOFT_AP,
+        WIFIMGR_DISCONNECTED_REQUESTED,
+        WIFIMGR_DISCONNECTED_LOSS,
+    };
+    int32_t _state;
+
+    std::atomic_bool _time_management;
+    bool _time_initialized;
+
+    int32_t _gmt_offset;
+    int32_t _daylight_offset;
+    String _ntp_server_1;
+    String _ntp_server_2;
+    String _ntp_server_3;
+};
+
+extern WiFiMgr WiFiManager;
+
+#endif
